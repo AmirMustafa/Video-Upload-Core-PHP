eb1ece717f15508972a61b0879a618d26e1d775d
Amir Mustafa <amirengg15@gmail.com>
2017-04-30 11:50:30 +0530

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 session_start();
 include("connect.php");
 
-if(isset($_REQUEST['videoSubmit'])){
+if(isset($_REQUEST['videoSubmit'])){		//receiving video files
 	//echo "Form submitted";
     
 	$videoTitle = $_REQUEST['title'];
@@ -11,7 +11,7 @@ if(isset($_REQUEST['videoSubmit'])){
 
 	//echo $_FILES["videoFile"]["name"];
 	
-	if($_FILES["videoFile"]["name"] != ''){
+	if($_FILES["videoFile"]["name"] != ''){ 	//if receives file
 	    $fileSize = $_FILES['videoFile']['size'];
 	    $fileType = $_FILES['videoFile']['type'];
 	    $fileName = str_shuffle('nityanandamaity').'-'.basename($_FILES["videoFile"]["name"]);
@@ -22,7 +22,7 @@ if(isset($_REQUEST['videoSubmit'])){
 
 		// echo $a = in_array($fileType, $allowedTypeArr); die;
 
-		if(in_array($fileType, $allowedTypeArr)) {echo "all good";
+		if(in_array($fileType, $allowedTypeArr)) {  //if above format video exists
 		    if(move_uploaded_file($_FILES['videoFile']['tmp_name'], $targetFile)) {
 		        $videoFilePath = $targetFile;
 		    }else{
@@ -44,11 +44,12 @@ if(isset($_REQUEST['videoSubmit'])){
     $query = "insert into videos(video_title,video_description,video_tags,video_path) values('$videoTitle','$videoDesc','$videoTags','$fileName')";
 
     // echo $query;die;
-    $run = mysqli_query($conn,$query);
+    $run = mysqli_query($conn,$query);		//executing above query
     
     if($run)
     {
         echo ("<script>alert('Video Uploaded Successfully.')</script>");
+        echo ("<script>window.location = 'index.php'</script>");
     }
     else
     {
